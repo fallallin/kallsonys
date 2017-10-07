a6840adf0f8a08796679d5aa66ddcae1177b0fd3
Oscar Huertas <tashuer@gmail.com>
2017-10-07 08:41:46 -0500

    Librerias de mule
    Librerias de mule

--- esb/app/external/Payment/build/generated-sources/ap-source-output/co/com/payment/model/Creditcard_.java
+++ esb/app/external/Payment/build/generated-sources/ap-source-output/co/com/payment/model/Creditcard_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-02T02:18:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-04T16:33:47")
 @StaticMetamodel(Creditcard.class)
 public class Creditcard_ { 
 
